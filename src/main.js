2c94c586c79e175d799177953451a618d52d7091
unknown <richmond.c@blueprintbusinesssolutionscorp.com>
2021-05-24 22:18:10 +0800

    updated

--- src/main.js
+++ src/main.js
@@ -4,19 +4,19 @@ import router from './router'
 import store from './store'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
+// import Echo from 'laravel-echo';
+
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.VUE_APP_WEBSOCKETS_KEY,
+//     wsHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
+//     wsPort: 6001,
+//     forceTLS: false,
+//     disableStats: true
+// });
 
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL
-import Echo from 'laravel-echo';
-
-window.Pusher = require('pusher-js');
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.VUE_APP_WEBSOCKETS_KEY,
-    wsHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true
-});
 
 createApp(App).use(store).use(router).mount('#app')
